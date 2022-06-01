137dce4d55cb13fccb49f9d69c1aa39cc7363658
Márcio Souza Júnior <marciosouzajunior@gmail.com>
2022-06-01 16:38:27 +0000

    Just coding ;)

--- build_exec.sh
+++ build_exec.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+  echo "No arguments supplied"
+  exit 1
+fi
+
 source=$1
 output=${source::(-1)}o
 binary=${source::(-2)}
--- find_number.s
+++ find_number.s
@@ -1,37 +1,46 @@
- .section .data
+# prints the number if exists in list, otherwise prints 0
 
-numbers_list:
- .long 1,2,3,4,5,6,7,8,9,10
+.section .data
+	numbers_list:
+	.long 1,2,3,4,5,6,7,8,9,10
 
-StringToPrint:
- .string "Hello"
+	number_to_search:
+	.long 5
 
- .section .text
+.section .bss
+	.lcomm charToPrint, 4
 
- .globl _start
+.section .text
+
+.globl _start
 _start:
- movl $0, %edi
- movl $0, %ebx
+	movl $0, %edi
+	movl $0, %ecx
 
-# TODO: not finished yet. need to load values from list and compare.
 start_loop:
- cmpl $9, %edi
- jmp print_char
- je loop_exit
- mov $4, %ebx
- jmp loop_exit
+	cmpl $9, %edi
+	je print_char
+	movl numbers_list(,%edi,4), %eax
+	incl %edi
+	cmpl (number_to_search), %eax
+	jne start_loop
+	movl %eax, %ecx
+	jmp print_char
 
 loop_exit:
- movl $1, %eax
- int $0x80
-
-# code from stack overflow ;)
-# just for 'debug' porpuses
+	movl $1, %eax
+	int $0x80
 
 print_char:
-movl $4, %eax
-movl $1, %ebx
-movl $StringToPrint, %ecx
-movl $5, %edx
-int $0x80
-jmp loop_exit
+	# preparing to make a syscall, will print the value in ecx register
+	movl $4, %eax			# sys_write
+	movl $1, %ebx			# file descriptor (1 = screen)
+	movl $1, %edx			# length of string
+
+	#movl %edi, %ecx		# move the value to be printed
+	addl $0x30, %ecx		# add 30 hex for ascii
+	movl %ecx, [charToPrint]	# save in the buffer
+	movl $charToPrint, %ecx		# save the adrress of buffer in ecx
+
+	int $0x80
+	jmp loop_exit
